99c707a31407e858c0d31d853be446f8c0fbc005
KyJuStudent <kjudge15383@midmich.edu>
2024-12-02 06:31:37 -0500

    sound updates
    im tired, i dont remember what i did

--- Assets/Scripts/audio/BackgroundMusicController.cs
+++ Assets/Scripts/audio/BackgroundMusicController.cs
@@ -9,7 +9,6 @@ public class BackgroundMusicController : MonoBehaviour
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        //PlayMusic();
     }
 
     public void PlayMusic()
